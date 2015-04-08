7828f9ad0a7699e01ae2c8741808839ede76691a
Rypouš Sloní <srypous@gmail.com>
2015-04-08 12:15:12 +0200

    Bitmap drawing, Gdiplus

--- src_slotview/Win32Graphics.hpp
+++ src_slotview/Win32Graphics.hpp
@@ -1,4 +1,8 @@
+// link the Gdiplus library
+#pragma comment(lib,"gdiplus.lib")
+
 #include <windows.h>
+#include <GdiPlus.h>
 #include "src_common/Settings.hpp"
 #include "WinMain.h"
 #include "vs_slotview/resource.h"
@@ -14,20 +18,38 @@ class Win32Graphics
 	int symbolW, symbolH;
 	bool wasInitialized;
 	HBITMAP bmpSymbol[Settings::symbolCount];
+
+	Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+	ULONG_PTR gdiplusToken;
+	Gdiplus::Pen* penGrid;
 public:
 	Win32Graphics(int width, int height)
 		: width(width)
 		, height(height)
 		, wasInitialized(false)
 	{}
+	~Win32Graphics()
+	{
+		for (int i = 0; i < Settings::symbolCount; i++)
+			DeleteObject(this->bmpSymbol[i]);
+		delete this->penGrid;
+	}
 	void init()
 	{
+		// Load bitmaps
 		this->loadSymbols();
-		this->wasInitialized = true;
-		this->offsetX = 50;
-		this->offsetY = 50;
+		
+		// Set dimensions
 		this->symbolW = 88;
 		this->symbolH = 88;
+		this->offsetX = (this->width-Settings::reelCount*this->symbolW)/2;
+		this->offsetY = 50;
+
+		// Init Gdiplus
+		Gdiplus::GdiplusStartup(&this->gdiplusToken, &this->gdiplusStartupInput, NULL);
+		this->penGrid = new Gdiplus::Pen(Gdiplus::Color(255, 250, 250, 250), 1.0);
+
+		this->wasInitialized = true;
 	}
 private:
 	void loadSymbols()
@@ -41,17 +63,34 @@ private:
 		this->bmpSymbol[6] = LoadBitmap(WinMainGlobal::hInst, MAKEINTRESOURCE(SZ_SYMBOL7));
 		this->bmpSymbol[7] = LoadBitmap(WinMainGlobal::hInst, MAKEINTRESOURCE(SZ_SYMBOL8));
 	}
+
 public:
 	void paint(HDC hdc)
 	{
 		if (!this->wasInitialized)
 			return;
 
+		Gdiplus::Graphics graphics(hdc);
+		graphics.SetSmoothingMode(Gdiplus::SmoothingModeAntiAlias);
+
 		for (int i = 0; i<Settings::reelCount; i++)
 			for (int j=0; j<Settings::windowSize; j++)
 				DrawState(hdc, NULL, NULL, LPARAM(this->bmpSymbol[Random::gen(0, 7)]), 0
 					,this->offsetX + i * this->symbolW, this->offsetY + j * this->symbolH
 					, 0, 0, DST_BITMAP);
+
+		for (int j=0; j<=Settings::windowSize; j++)
+		{
+			graphics.DrawLine(this->penGrid
+				, this->offsetX, this->offsetY + j * this->symbolH
+				, this->offsetX + Settings::reelCount * this->symbolW, this->offsetY + j * this->symbolH);
+		}
+		for (int i = 0; i<=Settings::reelCount; i++)
+		{
+				graphics.DrawLine(this->penGrid
+				, this->offsetX + i * this->symbolW, this->offsetY 
+				, this->offsetX + i * this->symbolW, this->offsetY + Settings::windowSize * this->symbolH);
+		}
 
 	}
 };
