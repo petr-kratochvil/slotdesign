187fb15aad4b0dcac8ffd01cabadb2eb27e8d9a4
Rypouš Sloní <srypous@gmail.com>
2015-04-11 22:16:23 +0200

    Statistics: computing variance

--- src_common/Statistics.hpp
+++ src_common/Statistics.hpp
@@ -6,21 +6,36 @@
 // Averaging (and more statistical stuff) of one value
 class StatItem
 {
+	// sum of data
 	long data;
+	// sum of (data squared)
+	long dataSquared;
+	// data items count
 	long count;
 public:
 	StatItem()
 		: data(0)
+		, dataSquared(0)
 		, count(0)
 	{}
 	void addData(int d)
 	{
 		this->data += d;
+		this->dataSquared += d * d;
 		this->count++;
 	}
+	// data average (mean value)
 	double getAvg() const
 	{
 		return double(this->data)/double(this->count);
+	}
+	// data variance
+	double getVar() const
+	{
+		// var X = E (X^2) - (E X)^2
+		return double(this->dataSquared) / double(this->count)
+				- double(this->data) * double(this->data)
+				/ ( double(this->count) * double(this->count) );
 	}
 	double getPct(long base) const
 	{
@@ -34,13 +49,6 @@ public:
 	{
 		return this->count;
 	}
-};
-
-// StatItem that stores all values, for example to compute variance afterwards
-class StatItemStore : StatItem
-{
-	int* allData;
-	int dataCount;
 };
 
 // Contains all global statistics from the simulation
